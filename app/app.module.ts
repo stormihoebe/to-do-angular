d1ab8c482c2b088f31068a4ba3a495f383859d59
Stormi Hoebelheinrich and Xi Xia <xi@>
2017-05-01 15:26:03 -0700

    completed to do angular app from course work

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }  from '@angular/forms';
+import { AppComponent }   from './app.component';
+import {TaskListComponent} from './task-list.component';
+import { EditTaskComponent} from './edit-task.component';
+import {NewTaskComponent} from './new-task.component';
+import { CompletenessPipe} from './completeness.pipe';
+
+@NgModule({
+  imports: [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, TaskListComponent, EditTaskComponent, NewTaskComponent, CompletenessPipe ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule {}
